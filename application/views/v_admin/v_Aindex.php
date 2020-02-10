b9f19e8092550488c3c1658a1e7ba2d9861e7971
hendrosusanto926 <hendrosusanto926@gmail.com>
2020-02-10 20:47:46 +0700

    hapus Komentar

--- application/views/v_admin/v_Aindex.php
+++ application/views/v_admin/v_Aindex.php
@@ -4,10 +4,7 @@
 <?php $this->load->view('header');?> 
 
 <body>
-    <!--[if lt IE 8]>
-		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-	<![endif]-->
-    <!-- Start Left menu area -->
+
     <?php $this->load->view('v_admin/v_menuleft');?> 
     <!-- End Left menu area -->
     <!-- Start Welcome area -->
